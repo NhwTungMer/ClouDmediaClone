c9511d880ef33aef9160c2791be7ac3c50f7dfab
wiler202 <107388826+NhwTungMer@users.noreply.github.com>
2024-10-07 21:13:27 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -51,7 +51,9 @@ app.post("/posts", verifyToken, upload.single("picture"), createPost);
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
-
+app.get("/landing",(req,res)=>{
+  res.status(200).send("landing Page")
+})
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
 mongoose
